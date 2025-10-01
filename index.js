8b33616bc5054dee26efa2d32d6dbf8390dbb65a
acrojason <acrojason77@gmail.com>
2025-09-30 20:04:29 -0700

    Update index.js
    Switch to export async function call instead

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let ST;
-async function init(api) {
-  ST = api;
+// ESM export: ST dynamically imports this and calls init()
+export async function init(api) {
   console.log('[TS-MW] init called');
 
-  ST.registerHook('message:send', async (payload, next) => {
+  // Prove the hook triggers when you send a message
+  api.registerHook('message:send', async (payload, next) => {
     console.log('[TS-MW] message:send type=%s text=%s',
-      payload?.type, (payload?.text||'').slice(0, 120));
+      payload?.type, (payload?.text || '').slice(0, 120));
     return next(payload);
   });
 
   return { success: true, message: 'TS MW loaded' };
 }
-module.exports = { init };
+
